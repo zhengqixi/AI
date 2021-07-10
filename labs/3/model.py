3cd0f8bb377c25e83b1a5c652994db7f19cebb05
Zhengqi Xi <zhengqixi@gmail.com>
2021-07-10 19:33:17 -0400

    Do solver

--- labs/3/model.py
+++ labs/3/model.py
@@ -1,74 +1,76 @@
 from typing import Dict, List, Tuple
 import numpy.typing as npt
+
 import numpy as np
+
+
+class Node:
+
+    def __init__(self, name: str) -> None:
+        self._name = name
+        self._reward = 0
+        self._edges = []
+        self._transition_probabilities = []
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def reward(self) -> float:
+        return self._reward
+
+    @reward.setter
+    def reward(self, reward: float) -> None:
+        self._reward = reward
+
+    def calculate_transition_probabilities(self, policy: Dict[str, str]) -> List[Tuple[str, float]]:
+        if self.is_decision:
+            success_prob = self._transition_probabilities[0]
+            fail_prob_per_edge = (1 - success_prob) / (len(self._edges) - 1)
+            transition_to_edge = policy[self._name]
+            transition_to_edge_index = self._edges.index(transition_to_edge)
+            transition_probabilities = [
+                fail_prob_per_edge for _ in self._edges]
+            transition_probabilities[transition_to_edge_index] = success_prob
+            return [x for x in zip(self._edges, transition_probabilities)]
+        return [x for x in zip(self._edges, self._transition_probabilities)]
+
+    @property
+    def edges(self) -> List[str]:
+        return self._edges
+
+    @edges.setter
+    def edges(self, edges: List[str]) -> None:
+        self._edges = edges
+
+    @property
+    def transition_probabilities(self) -> List[float]:
+        return self._transition_probabilities
+
+    @transition_probabilities.setter
+    def transition_probabilities(self, prob: List[float]) -> None:
+        self._transition_probabilities = prob
+
+    @property
+    def is_decision(self) -> bool:
+        return len(self._transition_probabilities) == 1 and len(self._edges) > 1
+
+    @property
+    def is_terminal(self) -> bool:
+        return len(self._edges) == 0
+
 
 class TransitionModel:
 
-    class Node:
-
-        def __init__(self, name: str) -> None:
-            self._name = name
-            self._reward = 0
-            self._edges = []
-            self._transition_probabilities = []
-
-        @property
-        def name(self) -> str:
-            return self._name
-
-        @property
-        def reward(self) -> float:
-            return self._reward
-        
-        @reward.setter
-        def reward(self, reward: float) -> None:
-            self._reward = reward 
-
-        def calculate_transition_probabilities(self, policy: Dict[str, str]) -> List[Tuple[str, float]]:
-            if self.is_decision:
-                success_prob = self._transition_probabilities[0]
-                fail_prob_per_edge = (1 - success_prob) / (len(self._edges) - 1)
-                transition_to_edge = policy[self._name]
-                transition_to_edge_index = self._edges.index(transition_to_edge)
-                transition_probabilities = [fail_prob_per_edge for _ in self._edges] 
-                transition_probabilities[transition_to_edge_index] = success_prob
-                return [x for x in zip(self._edges, transition_probabilities)]
-            return [x for x in zip(self._edges, self._transition_probabilities)]
-
-        @property
-        def edges(self) -> List[str] :
-            return self._edges
-        
-        @edges.setter
-        def edges(self, edges: List[str]) -> None:
-            self._edges = edges
-        
-        @property
-        def transition_probabilities(self) -> List[float]:
-            return self._transition_probabilities
-        
-        @transition_probabilities.setter
-        def transition_probabilities(self, prob: List[float]) -> None:
-            self._transition_probabilities = prob
-        
-        @property
-        def is_decision(self) -> bool:
-            return len(self._transition_probabilities) == 1
-        
-        @property
-        def is_terminal(self) -> bool:
-            return len(self._edges) == 0
-        
-    
     def __init__(self, nodes: List[Node]) -> None:
         self._nodes = nodes
-        self._node_to_index = {x.name:i for i, x in enumerate(nodes)}
+        self._node_to_index = {x.name: i for i, x in enumerate(nodes)}
         self._rewards = np.array([x.reward for x in nodes])
-
 
     def probability_matrix(self, policy: Dict[str, str]) -> npt.NDArray:
         size = len(self._nodes)
-        matrix = np.zeros(shape=[size,size])
+        matrix = np.zeros(shape=[size, size])
         for i, row in enumerate(matrix):
             node = self._nodes[i]
             transition_prob = node.calculate_transition_probabilities(policy)
@@ -80,7 +82,7 @@ class TransitionModel:
     @property
     def rewards(self) -> npt.NDArray:
         return self._rewards
-    
+
     @property
     def nodes(self) -> List[Node]:
         return self._nodes
@@ -94,11 +96,18 @@ class TransitionModel:
         rewards = [x.split('=') for x in lines if '=' in x]
         edges = [x.split(':') for x in lines if ':' in x]
         probabilities = [x.split('%') for x in lines if '%' in x]
-        rewards= [[x[0].strip(), x[1].strip()] for x in rewards]
-        edges = [[x[0].strip(), x[1].strip()] for x in edges]
-        probabilities = [[x[0].strip(), x[1].strip()] for x in probabilities]
+        rewards = [(x[0].strip(), x[1].strip()) for x in rewards]
+        edges = [(x[0].strip(), x[1].strip()) for x in edges]
+        probabilities = [(x[0].strip(), x[1].strip()) for x in probabilities]
         all_nodes = set([x[0] for x in (rewards + edges + probabilities)])
-        nodes = {x: TransitionModel.Node(x) for x in all_nodes}
+        nodes = {x: Node(x) for x in all_nodes}
+        for edge in edges:
+            node = edge[0]
+            if node not in nodes:
+                continue
+            edge_parsed = [x.strip() for x in edge[1][1:-1].split(',')]
+            edge_parsed = [x for x in edge_parsed if not x.isspace()]
+            nodes[node].edges = edge_parsed
         for reward in rewards:
             node = reward[0]
             if node not in nodes:
@@ -109,19 +118,18 @@ class TransitionModel:
             node = probability[0]
             if node not in nodes:
                 continue
-            transitional_probabilities = [float(x) for x in probability[1].split()]
+            transitional_probabilities = [
+                float(x) for x in probability[1].split()]
             nodes[node].transition_probabilities = transitional_probabilities
-        for edge in edges:
-            node = edge[0]
-            if node not in nodes:
-                continue
-            edge_parsed = [x.strip() for x in edge[1][1:-1].split(',')]
-            edge_parsed = [x for x in edge_parsed if not x.isspace()]
-            nodes[node].edges = edge_parsed
-        # Iterate through all the nodes and do validation
         nodes_list = [x for x in nodes.values()]
-        nodes_list.sort(key=lambda x : x.name)
+        nodes_list.sort(key=lambda x: x.name)
+        # If a node has edges but no probability entry, it is assumed to be a decision node with p=1
+        for node in nodes_list:
+            if len(node.transition_probabilities) == 0 and len(node.edges) > 0:
+                node.transition_probabilities = [1.0]
+        # Iterate through all the nodes and do validation
         return TransitionModel(nodes_list)
+
 
 if __name__ == '__main__':
     model = TransitionModel.from_file('test1.test')
---
+++ labs/3/solver.py
@@ -0,0 +1,73 @@
+from typing import Dict, Tuple, List
+import numpy.typing as npt
+import numpy as np
+from model import TransitionModel, Node
+
+
+class Solver:
+
+    def __init__(self, model: TransitionModel, discount: float, value_iter: int, value_tolerance: float, min: bool) -> None:
+        self._discount = discount
+        self._model = model
+        self._value_iter = value_iter
+        self._value_tolerance = value_tolerance
+        self._min = min
+
+    def solve(self) -> Tuple[Dict[str, str], npt.NDArray]:
+        nodes = self._model.nodes
+        nodes_to_index = {x.name: i for i, x in enumerate(nodes)}
+        v = self._model.rewards
+        decision_nodes = [x for x in nodes if x.is_decision]
+        # Initial policy is arbitrary
+        policy = {x.name: x.edges[0] for x in decision_nodes}
+        while True:
+            v = self._value_iterate(policy, v)
+            updated_policy = False
+            for node in decision_nodes:
+                current_policy = policy[node.name]
+                best_policy = self._calculate_best_policy(
+                    node, v, nodes_to_index)
+                if current_policy != best_policy:
+                    policy[node.name] = best_policy
+                    updated_policy = True
+
+            if not updated_policy:
+                return [policy, v]
+
+    def _value_iterate(self, policy: Dict[str, str], v: npt.NDArray) -> npt.NDArray:
+        probability_matrix = self._model.probability_matrix(policy)
+        rewards = self._model.rewards
+        new_v = v
+        for _ in range(self._value_iter):
+            new_v = rewards + self._discount * np.matmul(probability_matrix, v)
+            max_diff = np.max(np.absolute(new_v))
+            if max_diff <= self._value_tolerance:
+                return new_v
+            v = new_v
+        return new_v
+
+    def _calculate_best_policy(self, node: Node, v: List[float], nodes_to_index: Dict[str, int]) -> str:
+        success_prob = node.transition_probabilities[0]
+        fail_prob_per_edge = (1 - success_prob) / (len(node.edges) - 1)
+        rewards = []
+        for policy in node.edges:
+            policy_reward = success_prob * v[nodes_to_index[policy]]
+            other_rewards = node.edges.copy()
+            other_rewards.remove(policy)
+            other_rewards_sum = [v[nodes_to_index[x]] *
+                                 fail_prob_per_edge for x in other_rewards]
+            rewards.append((policy, policy_reward + sum(other_rewards_sum)))
+        if self._min:
+            return min(rewards, key=lambda x: x[1])[0]
+        return max(rewards, key=lambda x: x[1])[0]
+
+
+if __name__ == '__main__':
+    test_file = 'test7.test'
+    model = TransitionModel.from_file(test_file)
+    solver = Solver(model, 0.9, 200, 0.001, False)
+    result = solver.solve()
+    print(result[0])
+    nodes = model.nodes
+    for i, node in enumerate(nodes):
+        print('{}:{}'.format(node.name, result[1][i]))  # type: ignore
